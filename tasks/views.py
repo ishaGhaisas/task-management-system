c26f0a859eb3d85d82acf16e90d6e7a9b3041554
Isha Ghaisas <ighaisas@iu.edu>
2024-03-13 02:18:07 -0400

    idk why the styles are not working im giving up

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,16 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Tasks
 
 def tasks(request):
-    template =  loader.get_template('index.html')
-    return HttpResponse(template.render())
+    myTasks = Tasks.objects.all().values()
+    template =  loader.get_template('allTasks.html')
+    context = {
+        'myTasks' : myTasks
+    }
+    return HttpResponse(template.render(context, request))
 
 
-# Create your views here.
+def index(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
